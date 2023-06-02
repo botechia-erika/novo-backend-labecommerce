a6f5429df13624b8db424024f28430e65b638aee
mendoncabotechia@gmail.com <mendoncabotechia@proton.me>
2023-06-02 17:03:00 -0300

    node-package-json

--- src/endpoints/users/createUsers.ts
+++ src/endpoints/users/createUsers.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-     const createUser ={id ,name, email, password}
     } catch (error) {
 
     }
---
+++ src/types/TProducts.ts
@@ -0,0 +1,7 @@
+export type TProducts = {
+    id :string,
+    name :string,
+    price :string,
+    description: string,
+    image_url: string
+}
